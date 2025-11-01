bc0421cfc71084ecf40edc5b2238cde551376004
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2025-11-01 10:46:35 +0100

    add path logic

--- src/core/game.js
+++ src/core/game.js
@@ -98,6 +98,7 @@ class Game {
     // Navigation
     this.#events.on("go", (payload) => {
       this.#movement.go(payload);
+      this.runStep();
     });
 
     // Advance time if the effect payload carries a time cost
